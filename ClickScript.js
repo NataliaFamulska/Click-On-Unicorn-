2d96e8e883bbfb054929442b51a3b19830c6093b
Natalia <46263509+NataliaFamulska@users.noreply.github.com>
2019-09-11 12:14:45 +0200

    Add plusFive elements

--- ClickScript.js
+++ ClickScript.js
@@ -1,23 +1,40 @@
+//  basic 
 let buttonClick = document.getElementById("clickerButton");
+let displayValue = document.getElementById("addValue");
+// numbers
+let addValue = 1;
+let value = 1;
+let count = 0;
+// appearing elements plus2
 let appearingElement = document.getElementById("twoMore");
 let buttonPlusTwo = document.getElementById("addTwo");
 let costInfo = document.getElementById("costInfo");
-let addValue = 1;
-let value = 1;
 let cost = 5;
-let count = 0;
+//apearing elements plus5
+let appearingElementFive = document.getElementById("fiveMore");
+let buttonPlusFive = document.getElementById("addFive");
+let costInfoFive = document.getElementById("costInfoFive");
+let costFive = 10;
 
+
+// main function
 function click() {
   
   buttonClick.innerText = value;
   value+=addValue; 
+  displayValue.innerText = "You add " + addValue;
   
   if(appearingElement.classList.contains("nonVisible") && value == 10) {
     appearingElement.classList.remove("nonVisible");
     appearingElement.classList.toggle("visible");
-  } 
+  }
+  else if(appearingElementFive.classList.contains("nonVisible") && value == 50) {
+    appearingElementFive.classList.remove("nonVisible");
+    appearingElementFive.classList.toggle("visible");
+  }
 }
 
+//function plus2
 function valueTwoMore() {
   
   if(value > cost){
@@ -37,10 +54,35 @@ function valueTwoMore() {
    costInfo.innerText = "It's cost " + cost;
     
   }
+  
+  displayValue.innerText =  "You add " + addValue;
 }
 
+//function plus 5
+function valueFiveMore() {
+  
+  if(value > costFive){
+    
+   value-=costFive;
+   addValue+=5;
+   costFive+=10;
+    
+   buttonClick.innerText = value;
+
+   costInfoFive.innerText = "It's cost " + costFive;
+    
+  }
+  
+  displayValue.innerText =  "You add " + addValue;
+}
+
+
+//events
  buttonPlusTwo.addEventListener("click", valueTwoMore);
+ buttonPlusFive.addEventListener("click", valueFiveMore);
  buttonClick.addEventListener("click", click);
+
+
 
 
 
